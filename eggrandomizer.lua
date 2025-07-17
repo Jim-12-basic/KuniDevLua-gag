6202f53ca5914ce44ecda3a110e2bb2cf48625e1
TIMBERWOLVES <masteredge454@gmail.com>
2025-07-17 12:46:09 +0800

    Update eggrandomizer.lua

--- eggrandomizer.lua
+++ eggrandomizer.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KuniDevLua/gag/refs/heads/main/eggrandomizer.lua"))()
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/grgV8smvEF/0", true))()
+
